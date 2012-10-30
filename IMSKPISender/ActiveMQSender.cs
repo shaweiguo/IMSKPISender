d9a8011772bbfcdefacd3e7c55ce74763cad09f9
Sha Weiguo <shaweiguo@sohu.com>
2012-10-30 23:06:35 +0800

    v0.1

---
+++ IMSKPISender/ActiveMQSender.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace IMSKPISender
+{
+    public class ActiveMQSender
+    {
+        private readonly string _URI;
+        private readonly string _QueueName;
+        private readonly Random rdn = new Random();
+
+        public ActiveMQSender(string uri, string queuename)
+        {
+            _URI = uri;
+            _QueueName = queuename;
+        }
+
+        public class SendEventOccurred_Args : EventArgs
+        {
+            public string msg;
+            public SendEventOccurred_Args(string msg)
+            {
+                this.msg = msg;
+            }
+        }
+        public event EventHandler SendEventOccurred;
+
+        public bool SendKpi(string kpiname, string kpivalue, string orgappname, string developername)
+        {
+            Dictionary<string, string> message = new Dictionary<string, string>();
+            try
+            {
+                message.Add("CLASSNAME", "BusinessSystem");
+
+                //需要发送指标所属的系统名称;
+                message.Add("MAINDATA", "Name=" + orgappname);
+
+                message.Add(kpiname, kpivalue);
+
+                message.Add("TIME", String.Format("{0:yyyy-MM-dd HH:mm:ss}", System.DateTime.Now));
+
+                message.Add("SCENE", developername);
+
+                SendMessage(message);
+                message.Clear();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void SendMessage(string key, string value)
+        {
+            //Create the Connection Factory
+            string msgKey, msgValue;
+            using (IConnection connection = new ConnectionFactory(this._URI).CreateConnection())
+            {
+                using (ISession session = connection.CreateSession())
+                {
+                    using (IMessageProducer prod = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(this._QueueName)))
+                    {
+                        IMapMessage mapMessage = prod.CreateMapMessage();
+                        msgKey = "CLASSNAME";
+                        msgValue = "BusinessSystem";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "MAINDATA";
+                        msgValue = "Name=生产管理系统";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemOperationHandlingNUM";
+                        msgValue = "0";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessVisitCount";
+                        msgValue = "1234567";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemWorkOrderHandlingNUM";
+                        msgValue = "0";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemDBTime";
+                        msgValue = "1.05";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemOnlineNum";
+                        msgValue = "88";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemRunningTime";
+                        msgValue = "555555";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessDayLoginNum";
+                        msgValue = "99";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemSessionNum";
+                        msgValue = "90";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "BusinessSystemResponseTime";
+                        msgValue = "42";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "TIME";
+                        msgValue = String.Format("{0:yyyy-MM-dd HH:mm:ss}", System.DateTime.Now);
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        msgKey = "SCENE";
+                        msgValue = "NARI";
+                        mapMessage.Body.SetBytes(msgKey, Encoding.Default.GetBytes(msgValue));
+
+                        prod.Send(mapMessage);
+                        if (SendEventOccurred != null)
+                        {
+                            SendEventOccurred(this, new SendEventOccurred_Args(msgKey + "=" + msgValue + " 已发送."));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SendMessage(Dictionary<string, string> message)
+        {
+            //Create the Connection Factory
+            //string msgKey, msgValue;
+            using (IConnection connection = new ConnectionFactory(this._URI).CreateConnection())
+            {
+                using (ISession session = connection.CreateSession())
+                {
+                    using (IMessageProducer prod = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(this._QueueName)))
+                    {
+                        IMapMessage mapMessage = prod.CreateMapMessage();
+                        foreach (KeyValuePair<string, string> kpi in message)
+                        {
+                            mapMessage.Body.SetBytes(kpi.Key, Encoding.Default.GetBytes(kpi.Value));
+                        }
+
+                        prod.Send(mapMessage);
+                        if (SendEventOccurred != null)
+                        {
+                            SendEventOccurred(this, new SendEventOccurred_Args(" 已发送."));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- IMSKPISender/IMSKPISender.cs
+++ IMSKPISender/IMSKPISender.cs
@@ -7,17 +7,120 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Timers;
+using System.Data.Common;
 
 namespace IMSKPISender
 {
     public partial class IMSKPISendService : ServiceBase
     {
+        private string _DBConnStr = "";
+        private string _MQServerUri = "";
+        private string _QueueName = "";
+        private int _Interval = 1;
+        private Timer _Timer;
+        private DbProviderFactory _df;
+
         public IMSKPISendService()
         {
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args)
+        {
+            if (Init())
+            {
+                Log("IMSKPISender Started!");
+            }
+            else
+            {
+                Log("Init error.");
+            }
+        }
+
+        private bool Init()
+        {
+            try
+            {
+                _DBConnStr = ConfigurationManager.AppSettings["DBConnStr"].Trim();
+                _MQServerUri = ConfigurationManager.AppSettings["MQServerUri"].Trim();
+                _QueueName = ConfigurationManager.AppSettings["QueueName"].Trim();
+                _Interval = int.Parse(ConfigurationManager.AppSettings["Interval"].Trim());
+
+                _Timer = new Timer() { Interval = _Interval, AutoReset = true, Enabled = true };
+                _Timer.Elapsed += new ElapsedEventHandler(SendKpi);
+
+                Log("Init completed.");
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Log(ex.Message);
+                return false;
+            }
+        }
+
+        private DateTime Get5Minute0(DateTime t)
+        {
+            decimal m0 = t.Minute / 5;
+            decimal m = System.Math.Ceiling(m0 / 5);
+            return new DateTime(t.Year, t.Month, t.Day, t.Hour, (int)m, 0);
+        }
+        private DateTime Get5Minute1(DateTime t)
+        {
+            decimal m0 = t.Minute / 5;
+            decimal m = System.Math.Floor(m0 / 5);
+            return new DateTime(t.Year, t.Month, t.Day, t.Hour, (int)m, 0);
+        }
+
+        private void SendKpi(object source,ElapsedEventArgs e)
+        {
+            List<int> sendedKpiIds = new List<int>();
+
+            try
+            {
+                ActiveMQSender sender = new ActiveMQSender(this._MQServerUri, this._QueueName);
+                using (DbConnection cn = _df.CreateConnection())
+                {
+                    cn.ConnectionString = _DBConnStr;
+                    cn.Open();
+
+                    DbCommand cmdCheck = _df.CreateCommand();
+                    cmdCheck.Connection = cn;
+                    DateTime now = DateTime.Now;
+                    cmdCheck.CommandText=string.Format("Select * From [SendData] where [SendTime]>='{0}' and [SendTime]<'{1}'",
+                         Get5Minute0(now), Get5Minute1(now));
+                    using (DbDataReader dr = cmdCheck.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            if(sender.SendKpi(dr["KpiName"].ToString(), dr["KpiValue"].ToString(), dr["OrgappName"].ToString(), dr["DeveloperName"].ToString()))
+                                sendedKpiIds.Add(int.Parse(dr["Id"].ToString()));
+                        }
+                    }
+
+                    DbCommand cmdDelete = _df.CreateCommand();
+                    cmdDelete.Connection = cn;
+                    foreach (int id in sendedKpiIds)
+                    {
+                        cmdDelete.CommandText = "Delete From [SendData] Where [Id]=" + id;
+                        cmdDelete.ExecuteNonQuery();
+                    }
+
+
+                    cmdDelete.Dispose();
+                    cmdCheck.Dispose();
+                    cn.Close();
+                }
+
+            }
+            catch
+            {
+            }
+        }
+
+        private void Log(string msg)
         {
         }
 
